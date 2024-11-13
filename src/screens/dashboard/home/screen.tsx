11218806d20f8ff745140a7e9518c2167a99ae64
dalley8626 <senghuotlay86@gmail.com>
2024-11-13 23:02:37 +1100

    init

--- src/screens/dashboard/home/screen.tsx
+++ src/screens/dashboard/home/screen.tsx
@@ -2,6 +2,25 @@ import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import { useAuth } from "../../../context/auth/useAuth";
 
+// {"address": {"formattedAddress": "1 Downing St, Chicago, IL 60654, USA", "zoneId": "America/Chicago"}, "email": "jim.rose@gmail.com", "firstName": "Jim", "lastName": "Rose", "maxJobDistance": 20, "phoneNumber": "5096290220", "workerId": "7f90df6e-b832-44e2-b624-3143d428001f"}
+
+// type
+
+// {
+//   "address": {
+//     "formattedAddress": "1 Downing St, Chicago, IL 60654, USA",
+//     "zoneId": "America/Chicago"
+//   },
+//   "email": "jim.rose@gmail.com",
+//   "firstName": "Jim",
+//   "lastName": "Rose",
+//   "maxJobDistance": 20,
+//   "phoneNumber": "5096290220",
+//   "workerId": "7f90df6e-b832-44e2-b624-3143d428001f"
+// }
+
+// [{"branch": "Downtown", "branchPhoneNumber": "2531233322", "company": {"address": [Object], "name": "Steve Smith Construction", "reportTo": [Object]}, "jobId": "5775d8e18a488e6c5bb08333", "jobTitle": {"imageUrl": "https://imgs.swipejobs.com/js/job-category/construction-1.jpg", "name": "Construction General Helper"}, "milesToTravel": 3.4, "shifts": [[Object]], "wagePerHourInCents": 950}, {"branch": "Chicago", "branchPhoneNumber": "2531233311", "company": {"address": [Object], "name": "C.D. Barnes and Associates", "reportTo": [Object]}, "jobId": "5775d8e18a488e6c5bb08c13", "jobTitle": {"imageUrl": "https://imgs.swipejobs.com/js/job-category/driver-service-3.jpg", "name": "Driver"}, "milesToTravel": 11.666, "requirements": ["Safety Vest", "Hart Hat"], "shifts": [[Object], [Object], [Object], [Object], [Object], [Object], [Object], [Object], [Object], [Object]], "wagePerHourInCents": 1081.7}]
+
 // Why we're using export default?
 // Why not just arrow function, can also use const
 export default function HomeScreen() {
@@ -10,11 +29,22 @@ export default function HomeScreen() {
 
   useEffect(() => {
     const run = async () => {
-      const hello = await fetch(
+      const response = await fetch(
         `https://test.swipejobs.com/api/worker/${workerId}/profile`,
       );
 
+      const reponse2 = await fetch(
+        `https://test.swipejobs.com/api/worker/${workerId}/matches`,
+      );
+
+      const hello = await response.json();
+
+      const hello2 = await reponse2.json();
+
+      setList(hello);
+
       console.log("hello", hello);
+      console.log("hello2", hello2);
     };
 
     run();
@@ -22,7 +52,7 @@ export default function HomeScreen() {
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
+      <Text>Home Screen 1</Text>
     </View>
   );
 }
